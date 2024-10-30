9ca23b819fa0c7f9da191fd239c161aecd010cc3
MrA <abdou09nn@gmail.com>
2024-10-30 19:29:55 +0100

    cfff

--- suninternship/database/migrations/2024_07_05_081537_create_sessions_table.php
+++ suninternship/database/migrations/2024_07_05_081537_create_sessions_table.php
@@ -10,16 +10,19 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {
-        Schema::create('sessions', function (Blueprint $table) {
-            $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
-            $table->string('ip_address', 45)->nullable();
-            $table->text('user_agent')->nullable();
-            $table->longText('payload');
-            $table->integer('last_activity')->index();
-        });
-    }
+{
+    Schema::create('sessions', function (Blueprint $table) {
+        // Réduction de la longueur de la colonne id à 191 caractères
+        $table->string('id', 191)->primary(); // Spécifier la longueur ici
+        $table->foreignId('user_id')->nullable()->index();
+        $table->string('ip_address', 45)->nullable();
+        $table->text('user_agent')->nullable();
+        $table->longText('payload');
+        $table->integer('last_activity')->index();
+        $table->timestamps(); // Ajout de timestamps si nécessaire
+    });
+}
+
 
     /**
      * Reverse the migrations.
